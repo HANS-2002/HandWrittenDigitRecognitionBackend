5ad750044f97baec09e432333c501824cfb7ea05
hans-2002 <pattnaikhans@gmail.com>
2022-10-25 04:05:33 +0530

    Problem is openCV?

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-# import tensorflow as tf
+import tensorflow as tf
 # import cv2
-# from PIL import Image
-# import base64
-# import pickle
+from PIL import Image
+import base64
+import pickle
 from flask import Flask, request, jsonify
-# import numpy as np
+import numpy as np
 
 app = Flask(__name__)
 
